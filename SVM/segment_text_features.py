76e40ead461e590d7392ee7d945f45ea842ae47e
Bert & LIWC <81473905+M23A@users.noreply.github.com>
2025-06-27 10:54:41 +0100

    Update segment_text_features.py

--- SVM/segment_text_features.py
+++ SVM/segment_text_features.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.svm import SVC
-from sklearn.model_selection import StratifiedKFold, cross_val_score
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.model_selection import GroupKFold, cross_val_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from google.colab import drive
 
@@ -14,18 +14,17 @@ drive.mount('/content/drive')
 
 
 # Load the CSV file
-data = pd.read_csv('/content/drive/MyDrive/Maram/Text/Features_Extraction_from_text/Features_full_Text/LIWC/LIWC_fulltxt_with_labels.csv')
-
+data = pd.read_csv('/content/drive/MyDrive/Maram/Text/Features_Extraction_from_text/Features_chunk_Text/LIWC/LIWC-22-cleaned_Chtxt_with_labels.csv')
 # Separate features (X) and labels (y)
-X_combined = data.drop(['PID','CAI State'], axis=1).values  # Features without the label column
+X_combined = data.drop(['filename', 'CAI State'], axis=1).values
 y_combined = data['CAI State'].values  # Labels column
+# Define person_ids
+person_ids = data['filename']
 
-# Initialize the SVM model 
-svm_model = SVC(kernel='rbf',C=1 ,gamma='scale', probability=True)  
+# Initialize the SVM model
+svm_model = SVC(kernel='rbf',C=50, gamma='scale',probability=True)
 
-# Set up Stratified K-Fold Cross-Validation
-kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
-
+gkf = GroupKFold(n_splits=10)
 # Store metrics per fold
 accuracies = []
 precisions = []
@@ -35,11 +34,14 @@ confusion_matrices = []
 predictions = []
 true_labels = []
 
-for train_index, test_index in kf.split(X_combined, y_combined):
+for train_index, test_index in gkf.split(X_combined, y_combined, groups=person_ids):
+   # Split the data into training and testing sets for each fold
     X_train, X_test = X_combined[train_index], X_combined[test_index]
     y_train, y_test = y_combined[train_index], y_combined[test_index]
+
     # Initialize StandardScaler
     scaler = StandardScaler()
+
     # Scale the features (fit the scaler on the training set and transform both train and test data)
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
@@ -61,18 +63,20 @@ for train_index, test_index in kf.split(X_combined, y_combined):
     confusion_matrices.append(confusion_matrix(y_test, y_pred))
 
 
-print("Evaluation Results:")
+
+print("Evaluation Results (10-fold CV):")
 print(f"Accuracy:  {np.mean(accuracies):.2f} ± {np.std(accuracies):.2f}")
 print(f"Precision: {np.mean(precisions):.2f} ± {np.std(precisions):.2f}")
 print(f"Recall:    {np.mean(recalls):.2f} ± {np.std(recalls):.2f}")
 print(f"F1 Score:  {np.mean(f1s):.2f} ± {np.std(f1s):.2f}")
 
-# confusion matrices
+# Sum all confusion matrices across folds
 total_confusion = sum(confusion_matrices)
+
 print("\n Confusion Matrix:")
 print(total_confusion)
-import matplotlib.pyplot as plt
-import seaborn as sns
+
+
 plt.figure(figsize=(6, 5))
 sns.heatmap(total_confusion, annot=True, fmt='d', cmap='Blues',
             xticklabels=["Pred 0", "Pred 1"],
@@ -81,4 +85,3 @@ plt.title("Cumulative Confusion Matrix (Heatmap)")
 plt.xlabel("Predicted Label")
 plt.ylabel("True Label")
 plt.show()
-
